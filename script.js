acfe2e6ca0a93335e993438924a2e88468db413e
Anderson Moura <124076658+dados-repositorio@users.noreply.github.com>
2024-09-26 14:07:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Abrir e fechar o modal
+const contactButton = document.getElementById("contactButton");
+const modal = document.getElementById("contactModal");
+const closeModal = document.getElementsByClassName("close")[0];
+
+contactButton.onclick = function () {
+    modal.style.display = "block";
+};
+
+closeModal.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
